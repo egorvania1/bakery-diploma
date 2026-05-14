ade59600d923efbb27afe236e3a6de98867d8246
egorvania1 <egorvania1@gmail.com>
2026-05-14 22:02:15 +0500

    чистка кода

--- bakerysite/storage/models.py
+++ bakerysite/storage/models.py
@@ -4,28 +4,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 from accounts.models import Customer, Employee
 
-'''
-class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = PhoneNumberField(null=False, blank=False, unique=True)
-
-    class Meta:
-        unique_together = ('user', 'phone',)
-
-    def __str__(self):
-        return self.phone
-
-class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=30)
-
-    class Meta:
-        unique_together = ('user', 'role',)
-    
-    def __str__(self):
-        return str(self.user)
-'''
-
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     creation_date = models.DateTimeField()
